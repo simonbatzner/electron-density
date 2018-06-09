1a3a3575e0ab7c82f9d3650e51ebde1f8c49d27f
Simon Batzner <simonbatzner@gmail.com>
2018-06-09 15:59:47 +0200

    updated user input error handling

--- GP/gp.py
+++ GP/gp.py
@@ -18,20 +18,25 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 # suppress sklearn warnings
 def warn(*args, **kwargs):
     pass
+
+
 import warnings
+
 warnings.warn = warn
 
 from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.metrics import mean_absolute_error
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 from KRR_reproduce import *
 
 # setup
 ev2kcal = 1 / 0.043  # conversion factor
-SIM_NO = 150  # total number of data points
-M = 7 # number of training points
+sim_no = 150  # total number of data points
+m = 7  # number of training points
 np.random.seed(1)
 
 # path to data
@@ -42,7 +47,7 @@ STR_PREF = os.environ['PROJDIR'] + '/data/H2_DFT/temp_data/store/'
 ens = []
 seps = []
 
-for n in range(SIM_NO):
+for n in range(sim_no):
     # load separation, energy, and density
     sep = np.load(STR_PREF + 'sep_store/sep' + str(n) + '.npy')
     en = np.load(STR_PREF + 'en_store/en' + str(n) + '.npy')
@@ -58,11 +63,11 @@ for n in range(SIM_NO):
 data = seps
 labels = ens
 
-train_indices = [int(n) for n in np.round(np.linspace(0, 149, M))]
+train_indices = [int(n) for n in np.round(np.linspace(0, 149, m))]
 test_indices = [n for n in range(150) if n not in train_indices]
 
-if len(train_indices) != M:
-    print("Size of training set doesn't match the M specified")
+if len(train_indices) != m:
+    print("Size of training set doesn't match the m specified")
 
 x_train = np.array([data[n] for n in train_indices])
 x_test = np.array([data[n] for n in test_indices])
@@ -77,18 +82,26 @@ x_test = x_test.reshape(-1, 1)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 x_test_list = [data[n] for n in test_indices]
+x_train_list = [data[n] for n in train_indices]
 
 # build gp
-kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+# kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+kernel = RBF(10, (1e-2, 1e2))
 gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9, normalize_y=True)
 
 # fit
-print("Fitting GP...\n")
+print("\nFitting GP...")
 gp.fit(x_train, y_train)
 
 # inference
 y_pred_test = gp.predict(np.atleast_2d(x_test_list).T)
+y_pred_train = gp.predict(np.atleast_2d(x_train_list).T)
 y_pred_all, sigma = gp.predict(np.atleast_2d(seps).T, return_std=True)
+
+# print results
+print("\n=============================================")
+print("MAE on training data in [kcal/mol]: \t{}".format(mean_absolute_error(y_train, y_pred_train) * ev2kcal))
+print("MAE on test data in [kcal/mol]: \t\t{}".format(mean_absolute_error(y_test, y_pred_test) * ev2kcal))
 
 # plot w/ confidence intervals
 fig = plt.figure()
