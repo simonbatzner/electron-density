84b26faa85dcd5f506fb2df47a8f9836ce53ffc7
Simon Batzner <simonbatzner@gmail.com>
2018-06-13 09:52:32 -0400

    updated MD_engine integration

--- main.py
+++ main.py
@@ -19,84 +19,44 @@ from __future__ import print_function
 import os
 import argparse
 import numpy as np
-from matplotlib import pyplot as plt
 
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, Matern, ExpSineSquared
+from sklearn.gaussian_process.kernels import RBF
 
-import utility
+from utility import Atom, MD_engine
 
 
-def main():
-    """
-    Run adaptive MD using GP and uncertainty estimates
-    """
-
-    # params
-    kernel_choice = 'rbf'  # specify either c_rbf, rbf, matern or expsinesquared
-    m = 7  # number of training points
-
-    # setup
-    ev2kcal = 1 / 0.043  # conversion factor
-    sim_no = 150  # total number of data points
-
-    # path to data
-    os.environ['PROJDIR'] = '/Users/simonbatzner1/Desktop/Research/Research_Code/ML-electron-density'
-    STR_PREF = os.environ['PROJDIR'] + '/data/H2_DFT/temp_data/store/'
-
-    # load data
+def load_data(str_pref, sim_no):
+    pos = []
     ens = []
-    seps = []
 
     for n in range(sim_no):
-        # load separation, energy, and density
-        sep = np.load(STR_PREF + 'sep_store/sep' + str(n) + '.npy')
-        en = np.load(STR_PREF + 'en_store/en' + str(n) + '.npy')
+        # load arrays
+        en_curr = np.reshape(np.load(str_pref + 'en_store/energy' + str(n) + '.npy'), (1))[0]
+        pos_curr = np.load(str_pref + 'pos_store/pos' + str(n) + '.npy')
 
-        # format
-        sep = np.reshape(sep, (1,))[0]
-        en = np.reshape(en, (1,))[0]['energy']
-
-        ens.append(en)
-        seps.append(sep)
-
-    # set up training and test data
-    data = seps
-    labels = ens
-
-    train_indices = [int(n) for n in np.round(np.linspace(0, 149, m))]
-    test_indices = [n for n in range(150) if n not in train_indices]
-
-    if len(train_indices) != m:
-        print("Size of training set doesn't match the m specified")
-
-    x_train = np.array([data[n] for n in train_indices])
-    x_test = np.array([data[n] for n in test_indices])
-    y_train = np.array([labels[n] for n in train_indices])
-    y_test = np.array([labels[n] for n in test_indices])
+        # store arrays
+        ens.append(en_curr)
+        pos_curr = pos_curr.flatten()
+        pos.append(pos_curr)
 
     # convert to np arrays
-    x_train = np.array(x_train)
-    x_train = x_train.reshape(-1, 1)
-    x_test = np.array(x_test)
-    x_test = x_test.reshape(-1, 1)
-    y_train = np.array(y_train)
-    y_test = np.array(y_test)
-    x_test_list = [data[n] for n in test_indices]
-    x_train_list = [data[n] for n in train_indices]
+    ens = np.array(ens)
+    pos = np.array(pos)
 
-    # build gp w/ a noiseless kernel and print properties
-    kernel_dict = {
-        'c_rbf': C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2)),
-        'rbf': RBF(length_scale=10, length_scale_bounds=(1e-2, 1e2)),
-        'matern': Matern(length_scale=10, length_scale_bounds=(1e-2, 1e2),
-                         nu=10),
-        'expsinesquared': ExpSineSquared(length_scale=1.0, periodicity=3.0,
-                                         length_scale_bounds=(1e-2, 1e2),
-                                         periodicity_bounds=(1e-2, 1e2))}
+    alat = 4.10
+    Npos = 15
+    Atom1 = Atom(position=pos[Npos][:3] * alat, element='Al')
+    Atom2 = Atom(position=pos[Npos][3:6] * alat, element='Al')
+    Atom3 = Atom(position=pos[Npos][6:9] * alat, element='Al')
+    Atom4 = Atom(position=pos[Npos][9:] * alat, element='Al')
+    atoms = [Atom1, Atom2, Atom3, Atom4]
 
-    kernel = kernel_dict[kernel_choice]
+    return pos, ens, atoms
+
+
+def build_gp():
+    kernel = RBF(length_scale=10, length_scale_bounds=(1e-2, 1e2))
 
     print("\nKernel: {}".format(kernel))
     print("Hyperparameters: \n")
@@ -111,33 +71,53 @@ def main():
 
     gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=20, normalize_y=True)
 
+
+def main():
+    """
+    Run adaptive MD using GP and uncertainty estimates
+    """
+
+    # params
+    ev2kcal = 1 / 0.043
+    str_pref = os.environ['PROJDIR'] + '/data/H2_DFT/temp_data/store/'
+    sim_no = 201  # total number of data points
+
+    # scf params
+    pref = 'si'
+    pseudo_dir = '/n/home03/jonpvandermause/qe-6.2.1/pseudo'
+    pseudo_dir = '/Users/steven/Documents/Schoolwork/CDMAT275/ESPRESSO/qe-6.0'
+    outdir = '/n/home03/jonpvandermause/Cluster/Si_Supercell_SCF'
+    alat = 5.431  # lattice parameter of si in angstrom
+    ecut = 18.0  # plane wave cutoff energy
+    nk = 8  # size of kpoint grid
+    dim = 4  # size of supercell
+    nat = 2 * dim ** 3
+
+    pw_loc = '/n/home03/jonpvandermause/qe-6.2.1/bin/pw.x'
+    in_name = 'si.scf.in'
+    out_name = 'si.scf.out'
+    sh_name = 'Si_Super.sh'
+    partition = 'kaxiras'
+    memory = 1000
+    email = 'torrisi@g.harvard.edu'
+
+    # set up data
+    x_train, y_train, atoms = load_data(str_pref, sim_no)
+
+    # build gp model
+    gp = build_gp()
+
     # fit
     print("\nFitting GP...")
     gp.fit(x_train, y_train)
 
-    # inference
-    y_pred_test = gp.predict(np.atleast_2d(x_test_list).T)
-    y_pred_train = gp.predict(np.atleast_2d(x_train_list).T)
-    y_pred_all, sigma = gp.predict(np.atleast_2d(seps).T, return_std=True)
+    # build MD-ML engine
+    GP_engine = MD_engine(cell=alat * np.eye(3), input_atoms=atoms, ML_model=gp, model='KRR',
+                          store_trajectory=True, verbosity=4, assert_boundaries=False, dx=.1, fd_accuracy=4,
+                          threshold=0)
 
-    # print results
-    print("\n=============================================")
-    print("MAE on training data in [kcal/mol]: \t{}".format(mean_absolute_error(y_train, y_pred_train) * ev2kcal))
-    print("MAE on test data in [kcal/mol]: \t\t{}".format(mean_absolute_error(y_test, y_pred_test) * ev2kcal))
-
-    # plot w/ confidence intervals
-    plt.plot(x_train, y_train, 'r.', markersize=10, label=u'Training Data')
-    plt.plot(x_test, y_pred_test, 'b-', label=u'Predictions')
-    plt.fill(np.concatenate([seps, seps[::-1]]),
-             np.concatenate([y_pred_all - 1.9600 * sigma,
-                             (y_pred_all + 1.9600 * sigma)[::-1]]),
-             alpha=.5, fc='b', ec='None', label='95% confidence interval')
-
-    plt.xlabel('$x$')
-    plt.ylabel('$f(x)$')
-    plt.legend(loc='upper left')
-
-    plt.show()
+    # run
+    GP_engine.run(1, .1)
 
 
 if __name__ == '__main__':
