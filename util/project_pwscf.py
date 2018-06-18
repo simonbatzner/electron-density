03066438d6d1fc1154839d08d69d5b47b0123650
Steven T <torrisi@g.harvard.edu>
2018-06-18 15:49:45 -0700

    Added support for stronger parallelization options

--- util/project_pwscf.py
+++ util/project_pwscf.py
@@ -89,13 +89,21 @@ def write_pwscf_input(runpath, params, struc, kpoints, pseudopots, constraint=No
 	return infile
 
 
-def run_qe_pwscf(struc, runpath, pseudopots, params, kpoints, constraint=None, ncpu=1):
+def run_qe_pwscf(struc, runpath, pseudopots, params, kpoints, constraint=None, ncpu=1,parallelization={}):
 	pwscf_code = ExternalCode({'path': os.environ['PWSCF_COMMAND']})
 	prepare_dir(runpath.path)
 	infile = write_pwscf_input(params=params, struc=struc, kpoints=kpoints, runpath=runpath,
 							   pseudopots=pseudopots, constraint=constraint)
 	outfile = File({'path': os.path.join(runpath.path, 'pwscf.out')})
-	pwscf_command = "mpirun -np {} {} < {} > {}".format(ncpu, pwscf_code.path, infile.path, outfile.path)
+    
+	parallelization_str=""
+	if parallelization!={}:
+		for key,value in parallelization.items():
+			if value!=0:
+				parallelization_str+= '-%s %d '%(key,value)
+	else:
+		parallelization_str="-np %d"%(ncpu)
+	pwscf_command = "mpirun {} {} < {} > {}".format(parallelization_str, pwscf_code.path, infile.path, outfile.path)
 	run_command(pwscf_command)
 	return outfile
 
