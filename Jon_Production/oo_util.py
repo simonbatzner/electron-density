47ef354c0bc391c0787e284fcbbf8e568eee93e6
Simon Batzner <simonbatzner@gmail.com>
2018-07-19 00:52:06 -0400

    added homemade gp helper functions, some pep updates

--- Jon_Production/oo_util.py
+++ Jon_Production/oo_util.py
@@ -12,10 +12,13 @@ from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import GridSearchCV
 from sklearn.gaussian_process.kernels import RBF, Matern
 
+from Jon_Production.utility import get_SE_K, GP_SE_alpha, GP_SE_like
+
 
 class RegressionModel:
     """Base class for regression models"""
-    def __init__(self, model, training_data, training_labels, test_data, test_labels, model_type):
+
+    def __init__(self, model, training_data, training_labels, test_data, test_labels, model_type, verbosity):
         """
         Initialization
         """
@@ -25,24 +28,30 @@ class RegressionModel:
         self.test_data = test_data
         self.test_labels = test_labels
         self.model_type = type
+        self.verbosity = verbosity
 
     def train(self):
         """
         Train ML model on training_data/ training_labels
         """
+        if self.verbosity > 1:
+            print("Training model...")
         self.model.fit(self.training_data, self.training_labels)
 
     def inference(self):
         """
         Predict on test data
         """
-        self.model.predict(self.test_data)
+        if self.verbosity > 1:
+            print("Performing inference")
+        return self.model.predict(self.test_data)
 
 
 class GaussianProcess(RegressionModel):
     """Gaussian Process Regression Model"""
+
     def __init__(self, training_data, training_labels, test_data, test_labels, kernel, length_scale, length_scale_min,
-                 length_scale_max, n_restarts, sklearn):
+                 length_scale_max, n_restarts, sklearn, verbosity):
         """
         Initialization
         """
@@ -51,6 +60,7 @@ class GaussianProcess(RegressionModel):
         self.length_scale_max = length_scale_max
         self.n_restarts = n_restarts
         self.sklearn = sklearn
+        self.verbosity = verbosity
         self.kernel_dict = {'rbf': RBF(length_scale=self.length_scale,
                                        length_scale_bounds=(self.length_scale_min, self.length_scale_max)),
                             'matern_15': Matern(length_scale=self.length_scale,
@@ -69,7 +79,8 @@ class GaussianProcess(RegressionModel):
             self.model = VGP()
 
         RegressionModel.__init__(self, model=self.model, training_data=training_data, test_data=test_data,
-                                 training_labels=training_labels, test_labels=test_labels, model_type='gp')
+                                 training_labels=training_labels, test_labels=test_labels, model_type='gp',
+                                 verbosity=verbosity)
 
     def train(self):
         """
@@ -81,50 +92,54 @@ class GaussianProcess(RegressionModel):
         """
         Predict on test data
         """
-        super(GaussianProcess, self).inference()
+        return super(GaussianProcess, self).inference()
 
 
 class KernelRidgeRegression(RegressionModel):
     """KRR Regression Model"""
-    def __init__(self, training_data, training_labels, test_data, test_labels, sklearn, kernel,
-                 alpha_range, gamma_range):
+
+    def __init__(self, training_data, training_labels, test_data, test_labels, kernel,
+                 alpha_range, gamma_range, cv, sklearn, verbosity):
         """
         Initialization
         """
         self.alpha_range = alpha_range
         self.gamma_range = gamma_range
         self.kernel = kernel
+        self.verbosity = verbosity
         self.sklearn = sklearn
+        self.cv = cv
 
         if self.sklearn:
-            self.model = KernelRidge(kernel=kernel)
+            self.model = GridSearchCV(KernelRidge(kernel=self.kernel), cv=self.cv,
+                                      param_grid={"alpha": self.alpha_range,
+                                                  "gamma": self.gamma_range})
 
         else:
             # PyFly implementation of Kernel Ridge Regression
             self.model = KRR_PF
 
         RegressionModel.__init__(self, model=self.model, training_data=training_data, test_data=test_data,
-                                 training_labels=training_labels, test_labels=test_labels, model_type='krr')
+                                 training_labels=training_labels, test_labels=test_labels, model_type='krr',
+                                 verbosity=verbosity)
 
-    def train(self, alpha_range, gamma_range, cv):
+    def train(self):
         """
-        Train using gridsearch on KRR
+        Train ML model on training_data/ training_labels
         """
-        kr = GridSearchCV(KernelRidge(kernel=self.kernel), cv=cv,
-                          param_grid={"alpha": alpha_range,
-                                      "gamma": gamma_range})
-
-        kr.fit(self.training_data, self.training_labels)
+        super(KernelRidgeRegression, self).train()
 
     def inference(self):
         """
         Predict on test data
         """
-        super(KernelRidgeRegression, self).inference()
+        return super(KernelRidgeRegression, self).inference()
 
 
 class VGP():
-    """Vectorized Gaussian Process"""
+    """
+    PyFly implementation of a vectorial Gaussian Process
+    """
     pass
 
 
