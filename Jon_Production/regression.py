491985c968ed324752558ecda23d1240603edd8e
Simon Batzner <simonbatzner@gmail.com>
2018-07-21 00:50:43 -0400

    integrated jon's homemade gp with oop regression models

--- Jon_Production/regression.py
+++ Jon_Production/regression.py
@@ -7,9 +7,9 @@
 Simon Batzner
 """
 import os
+import copy
 
 import numpy as np
-
 from scipy.optimize import minimize
 
 from sklearn.gaussian_process import GaussianProcessRegressor
@@ -17,7 +17,7 @@ from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import GridSearchCV
 from sklearn.gaussian_process.kernels import RBF, Matern
 
-from Jon_Production.utility import get_SE_K, GP_SE_alpha, minus_like_hyp, GP_SE_pred, aug_and_norm
+from Jon_Production.utility import get_SE_K, GP_SE_alpha, minus_like_hyp, GP_SE_pred, symmetrize_forces
 from util.project_pwscf import parse_qe_pwscf_output
 
 
@@ -38,10 +38,33 @@ def get_files(root_dir):
     return matching
 
 
+def parse_output(filename, target):
+    """
+    Parse QE output file and return new datapoint as input_data, target
+    :param filename:            str, QE file to read from
+    :return: [data, target]     input config and target for ML model from QE output file
+    """
+    result = parse_qe_pwscf_output(filename)
+
+    if target == 'f':
+        positions, forces = result['positions'], result['forces']
+
+    elif target == 'e':
+        raise ValueError("Not implemented yet. Stay tuned.")
+
+    elif target == 'fe':
+        raise ValueError("Not implemented yet. Stay tuned.")
+
+    else:
+        raise ValueError("No proper ML target defined.")
+
+    return positions, forces
+
+
 class RegressionModel:
     """Base class for regression models"""
 
-    def __init__(self, model, training_data, training_labels, test_data, test_labels, correction_folder,
+    def __init__(self, model, training_data, test_data, correction_folder,
                  training_folder, model_type, target, verbosity):
         """
         Initialization
@@ -49,70 +72,119 @@ class RegressionModel:
         self.model = model
         self.target = target
         self.training_data = training_data
-        self.training_labels = training_labels
         self.test_data = test_data
-        self.test_labels = test_labels
         self.model_type = model_type
         self.verbosity = verbosity
         self.training_folder = training_folder
         self.correction_folder = correction_folder
         self.aug_files = []
 
-    def parse_output(self, filename):
-        """
-        Parse QE output file and return new datapoint as input_data, target
-        :param filename:            str, QE file to read from
-        :return: [data, target]     input config and target for ML model from QE output file
-        """
-        result = parse_qe_pwscf_output(filename)
-
-        if self.target == 'f':
-            labels, data = result['forces'], result['positions']
-
-        elif self.target == 'e':
-            raise ValueError("Not implemented yet. Stay tuned.")
-
-        elif self.target == 'fe':
-            raise ValueError("Not implemented yet. Stay tuned.")
-
-        else:
-            raise ValueError("No proper ML target defined.")
-
-        return data, labels
-
-    def upd_database(self):
+    def upd_database(self, cutoff, eta_lower, eta_upper, eta_length, brav_mat, brav_inv, vec1, vec2, vec3):
         """
         Add new training data from augmentation folder
         """
         for file in get_files(self.correction_folder):
+
             if file not in self.aug_files:
+
                 self.aug_files.append(file)
 
-                data, labels = self.parse_output(file)
+                positions, forces = parse_output(file, target=self.target)
 
-                for d, l in zip(data, labels):
-                    self.training_data.append(d)
-                    self.training_labels.append(l)
-
-                    # TODO: normalize database
+                for pos, f in zip(positions, forces):
+                    self.aug_and_norm(pos=pos, forces=f, cutoff=cutoff, eta_lower=eta_lower, eta_upper=eta_upper,
+                                      eta_length=eta_length, brav_mat=brav_mat, brav_inv=brav_inv, vec1=vec1,
+                                      vec2=vec2, vec3=vec3)
 
     def init_database(self):
         """
         Init training database from directory
         """
-        for file in get_files(self.training_folder):
+        pass
 
-            data, labels = self.parse_output(file)
+    def aug_and_norm(self, pos, forces, cutoff, eta_lower, eta_upper, eta_length,
+                     brav_mat, brav_inv, vec1, vec2, vec3):
+        """
+        Augment and normalize
+        """
 
-            for d, l in zip(data, labels):
-                self.training_data.append(d)
-                self.training_labels.append(l)
+        # augment
+        self.augment_database(pos, forces, cutoff, eta_lower, eta_upper, eta_length,
+                              brav_mat, brav_inv, vec1, vec2, vec3)
+
+        # normalize forces and symmetry vectors
+        self.normalize_force()
+        self.normalize_symm()
+
+    def augment_database(self, pos, forces, cutoff, eta_lower, eta_upper, eta_length,
+                         brav_mat, brav_inv, vec1, vec2, vec3):
+        """
+        For a given supercell, calculate symmetry vectors for each atom
+        """
+        for n in range(len(pos)):
+            # get symmetry vectors
+            symm_x, symm_y, symm_z = symmetrize_forces(pos, n, cutoff, eta_lower, eta_upper,
+                                                       eta_length, brav_mat, brav_inv, vec1, vec2, vec3)
+
+            # append symmetry vectors
+            self.training_data['symms'].append(symm_x)
+            self.training_data['symms'].append(symm_y)
+            self.training_data['symms'].append(symm_z)
+
+            # append force components
+            self.training_data['forces'].append(forces[n][0])
+            self.training_data['forces'].append(forces[n][1])
+            self.training_data['forces'].append(forces[n][2])
+
+    def normalize_symm(self):
+        """
+        Normalize the symmetry vectors in the training set
+        """
+        symm_len = len(self.training_data['symms'][0])
+        td_size = len(self.training_data['symms'])
+
+        # initialize normalized symmetry vector
+        self.training_data['symm_norm'] = copy.deepcopy(self.training_data['symms'])
+
+        # store normalization factors
+        self.training_data['symm_facs'] = []
+
+        for m in range(symm_len):
+            # calculate standard deviation of current symmetry element
+            vec = np.array([self.training_data['symms'][n][m] for n in range(td_size)])
+            vec_std = np.std(vec)
+
+            # store standard deviation
+            self.training_data['symm_facs'].append(vec_std)
+
+            # normalize the current element
+            for n in range(td_size):
+                self.training_data['symm_norm'][n][m] = self.training_data['symm_norm'][n][m] / vec_std
+
+    def normalize_force(self):
+        """
+        Normalize forces
+        """
+        td_size = len(self.training_data['forces'])
+
+        # initialize normalized force vector
+        self.training_data['forces_norm'] = copy.deepcopy(self.training_data['forces'])
+
+        # calculate standard deviation of force components
+        vec_std = np.std(self.training_data['forces'])
+
+        # store standard deviation
+        self.training_data['force_fac'] = vec_std
+
+        # normalize the forces
+        for n in range(td_size):
+            self.training_data['forces_norm'][n] = self.training_data['forces_norm'][n] / vec_std
 
 
 class GaussianProcess(RegressionModel):
     """Gaussian Process Regression Model"""
 
-    def __init__(self, training_data=None, training_labels=None, test_data=None, test_labels=None, kernel='rbf',
+    def __init__(self, training_data=None, test_data=None, kernel='rbf',
                  length_scale=1, length_scale_min=1e-5, length_scale_max=1e5, sigma=1, n_restarts=10,
                  correction_folder='.', training_folder='.', target='f', verbosity=1, sklearn=False):
         """
@@ -153,22 +225,21 @@ class GaussianProcess(RegressionModel):
             self.alpha = None
 
         RegressionModel.__init__(self, model=self.model, training_data=training_data, test_data=test_data,
-                                 training_labels=training_labels, test_labels=test_labels,
                                  correction_folder=correction_folder, training_folder=training_folder,
-                                 model_type='gp', target='f', verbosity=verbosity)
+                                 model_type='gp', target=target, verbosity=verbosity)
 
-    def retrain(self):
+    def retrain(self, cutoff, eta_lower, eta_upper, eta_length, brav_mat, brav_inv, vec1, vec2, vec3):
         """
         Retrain GP model in active learning procedure based on new training data
         """
-        self.upd_database()
+        self.upd_database(cutoff, eta_lower, eta_upper, eta_length, brav_mat, brav_inv, vec1, vec2, vec3)
         self.train()
 
     def opt_hyper(self):
         """
         Optimize hyperparameters by minimizing minus log likelihood w/ Nelder-Mead
         """
-        args = (self.training_data, self.training_labels)
+        args = (self.training_data['symm_norm'], self.training_data['forces_norm'])
 
         # initial guess
         x0 = np.array([self.sigma, self.length_scale])
@@ -183,17 +254,17 @@ class GaussianProcess(RegressionModel):
         Train ML model on training_data/ training_labels
         """
         if self.sklearn:
-            self.model.fit(self.training_data, self.training_labels)
+            self.model.fit(self.training_data['symm_norm'], self.training_data['forces_norm'])
 
         else:
             # optimize hyperparameters
             self.opt_hyper()
 
             # following: Algorithm 2.1 (pg. 19) of "Gaussian Processes for Machine Learning" by Rasmussen and Williams.
-            self.K, self.L = get_SE_K(self.training_data, self.sigma, self.length_scale)
+            self.K, self.L = get_SE_K(self.training_data['symm_norm'], self.sigma, self.length_scale)
 
             # get alpha and likelihood
-            self.alpha = GP_SE_alpha(self.K, self.L, self.training_data)
+            self.alpha = GP_SE_alpha(self.K, self.L, self.training_data['symm_norm'])
 
     def inference(self):
         """
@@ -202,11 +273,11 @@ class GaussianProcess(RegressionModel):
 
         if self.sklearn:
             # TODO: check this
-            self.pred, std = self.model.predict(self.test_data, return_std=True)
+            self.pred, std = self.model.predict(self.test_data['symm_norm'], return_std=True)
             self.pred_var = std ** 2
 
         else:
-            self.pred, self.pred_var = GP_SE_pred(self.training_data, self.training_labels, self.K,
+            self.pred, self.pred_var = GP_SE_pred(self.test_data['symm_norm'], self.test_data['forces_norm'], self.K,
                                                   self.L, self.alpha, self.sigma, self.length_scale, self.test_data)
 
 
@@ -235,7 +306,6 @@ class KernelRidgeRegression(RegressionModel):
             pass
 
         RegressionModel.__init__(self, model=self.model, training_data=training_data, test_data=test_data,
-                                 training_labels=training_labels, test_labels=test_labels,
                                  correction_folder=correction_folder, training_folder=training_folder,
                                  model_type='krr', target='f', verbosity=verbosity)
 
@@ -243,10 +313,11 @@ class KernelRidgeRegression(RegressionModel):
         """
         Train ML model on training_data/ training_labels
         """
-        self.model.fit(self.training_data, self.training_labels)
+        self.model.fit(self.training_data['symm_norm'], self.training_data['forces_norm'])
+
 
     def inference(self):
         """
         Predict on test data
         """
-        self.model.predict(self.test_data)
+        self.model.predict(self.test_data['symm_norm'])
