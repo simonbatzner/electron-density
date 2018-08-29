6c5c24a38bd90bf5e91b86848d1977218d137963
Simon Batzner <simonbatzner@gmail.com>
2018-08-29 11:57:47 -0400

    added init_db() method

--- OTF/src/gp.py
+++ OTF/src/gp.py
@@ -8,12 +8,125 @@ Implementation is based on Algorithm 2.1 (pg. 19) of
 
 Simon Batzner
 """
+import os
+
 import math
 
 import numpy as np
 from scipy.linalg import solve_triangular
 from scipy.optimize import minimize
-from two_body import two_body
+
+from ..two_body import two_body
+from otf import parse_qe_input, parse_qe_forces, Structure
+
+
+def get_outfiles(root_dir, out=True):
+    """
+    Find all files matching *.out OR *.in
+
+    :param root_dir:    (str), dir to walk from
+    :param out:         (bool), whether to look for .out (true) or .in files (false)
+    :return: matching   (list), files in root_dir ending with .out
+    """
+    matching = []
+
+    for root, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+
+            if out:
+                if filename.endswith('.out'):
+                    matching.append(os.path.join(root, filename))
+
+            else:
+                if filename.endswith('.in'):
+                    matching.append(os.path.join(root, filename))
+
+    return matching
+
+
+
+# given list of Cartesian coordinates, return list of atomic environments
+def get_cutoff_vecs(vec, brav_mat, brav_inv, vec1, vec2, vec3, cutoff):
+    # get bravais coefficients
+    coeff = np.matmul(brav_inv, vec)
+
+    # get bravais coefficients for atoms within one super-super-cell
+    coeffs = [[], [], []]
+    for n in range(3):
+        coeffs[n].append(coeff[n])
+        coeffs[n].append(coeff[n] - 1)
+        coeffs[n].append(coeff[n] + 1)
+        coeffs[n].append(coeff[n] - 2)
+        coeffs[n].append(coeff[n] + 2)
+
+    # get vectors within cutoff
+    vecs = []
+    dists = []
+    for m in range(len(coeffs[0])):
+        for n in range(len(coeffs[1])):
+            for p in range(len(coeffs[2])):
+                vec_curr = coeffs[0][m] * vec1 + coeffs[1][n] * vec2 + coeffs[2][p] * vec3
+
+                dist = np.linalg.norm(vec_curr)
+
+                if dist < cutoff:
+                    vecs.append(vec_curr)
+                    dists.append(dist)
+
+    return vecs, dists
+
+
+# given list of cartesian coordinates, get chemical environment of specified atom
+# pos = list of cartesian coordinates
+# typs = list of atom types
+def get_env_struc(pos, typs, atom, brav_mat, brav_inv, vec1, vec2, vec3, cutoff):
+    pos_atom = np.array(pos[atom]).reshape(3, 1)
+    typ = typs[atom]
+    env = {'central_atom': typ, 'dists': [], 'xs': [], 'ys': [], 'zs': [],
+           'xrel': [], 'yrel': [], 'zrel': [], 'types': []}
+
+    # loop through positions to find all atoms and images in the neighborhood
+    for n in range(len(pos)):
+        # position relative to reference atom
+        diff_curr = np.array(pos[n]).reshape(3, 1) - pos_atom
+
+        # get images within cutoff
+        vecs, dists = get_cutoff_vecs(diff_curr, brav_mat,
+                                      brav_inv, vec1, vec2, vec3, cutoff)
+
+        for vec, dist in zip(vecs, dists):
+            # ignore self interaction
+            if dist != 0:
+                # append distance
+                env['dists'].append(dist)
+
+                # append coordinate differences
+                env['xs'].append(vec[0][0])
+                env['ys'].append(vec[1][0])
+                env['zs'].append(vec[2][0])
+
+                # append relative coordinate differences
+                env['xrel'].append(vec[0][0] / dist)
+                env['yrel'].append(vec[1][0] / dist)
+                env['zrel'].append(vec[2][0] / dist)
+
+                # append atom type
+                env['types'].append(typs[n])
+
+    #env['trip_dict'] = get_trip_dict(env)
+
+    return env
+
+
+# given list of cartesian coordinates, return list of chemical environments
+def get_envs(pos, typs, brav_mat, brav_inv, vec1, vec2, vec3, cutoff):
+    envs = []
+    for n in range(len(pos)):
+        atom = n
+        env = get_env_struc(pos, typs, atom, brav_mat, brav_inv, vec1, vec2, vec3, cutoff)
+        envs.append(env)
+
+    return envs
 
 
 class GaussianProcess:
@@ -44,8 +157,24 @@ class GaussianProcess:
         self.alpha = None
 
         # training set
-        self.training_data = []
-        self.training_labels = []
+        self.training_data = np.empty(0,)
+        self.training_labels = np.empty(0,)
+
+        # initiate database
+        self.init_db()
+
+    def init_db(self, root_dir):
+        """Initialize database from root directory containing training data"""
+
+        for file in get_outfiles(root_dir=root_dir, out=False):
+            positions, species, cell = parse_qe_input(file)
+
+        for file in get_outfiles(root_dir=root_dir, out=True):
+            forces = parse_qe_forces(file)
+
+        self.training_data = np.asarray(get_envs(pos=positions, typs=['Si'], brav_mat=brav_mat, brav_inv=brav_inv,
+                                                 vec1=vec1, vec2=vec2, vec3=vec3, cutoff=cutoff))
+        self.training_labels = np.asarray(forces)
 
     def train(self):
         """
@@ -185,4 +314,27 @@ class GaussianProcess:
 
 
 if __name__ == "__main__":
-    gp = GaussianProcess(kernel=two_body())
+
+    global alat, brav_inv, brav_mat, vec1, vec2, vec3, cutoff
+
+    # set crystal structure
+    dim = 3
+    alat = 4.344404578
+    unit_cell = [[0.0, alat / 2, alat / 2], [alat / 2, 0.0, alat / 2], [alat / 2, alat / 2, 0.0]]
+    unit_pos = [['Si', [0, 0, 0]], ['Si', [alat / 4, alat / 4, alat / 4]]]
+    brav_mat = np.array([[0.0, alat / 2, alat / 2], [alat / 2, 0.0, alat / 2], [alat / 2, alat / 2, 0.0]]) * dim
+    brav_inv = np.linalg.inv(brav_mat)
+
+    # bravais vectors
+    vec1 = brav_mat[:, 0].reshape(3, 1)
+    vec2 = brav_mat[:, 1].reshape(3, 1)
+    vec3 = brav_mat[:, 2].reshape(3, 1)
+    cutoff = 4.5
+
+    positions, species, cell = parse_qe_input('pwscf.in')
+    forces = parse_qe_forces('pwscf.out')
+    structure = Structure(positions, species, cell)
+
+    gp = GaussianProcess(kernel=two_body)
+    gp.train()
+    gp.predict(structure)
